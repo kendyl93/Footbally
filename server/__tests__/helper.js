75dadc331632b71151b9fda47efbe68f52f1e9c3
Pawel Stanecki <p.stanecki93@gmail.com>
2020-08-25 22:18:49 +0200

    Fix excluding rules between prettier and eslint

--- server/__tests__/helper.js
+++ server/__tests__/helper.js
@@ -2,9 +2,9 @@ const supertest = require("supertest");
 const app = require("../app");
 
 class Helper {
-    constructor(model) {
-        this.apiServer = supertest(app);
-    }
+  constructor() {
+    this.apiServer = supertest(app);
+  }
 }
 
 module.exports = Helper;
--- server/__tests__/routes/users.test.js
+++ server/__tests__/routes/users.test.js
@@ -3,9 +3,9 @@ const helper = new Helper();
 const urlPrefix = "/api/v1/users";
 
 describe("Users enpoint", () => {
-    it("Get all users", async () => {
-        const { body } = await helper.apiServer.get(urlPrefix)
+  it("Get all users", async () => {
+    const { body } = await helper.apiServer.get(urlPrefix);
 
-        expect(body).toEqual({users: [{name: 'Timmy'}]});
-    });
+    expect(body).toEqual({ users: [{ name: "Timmy" }] });
+  });
 });

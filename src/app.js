3bc482c39eeeb1e9000ef45c842b77d7586f50e5
Pawel Stanecki <p.stanecki93@gmail.com>
2020-09-06 18:11:55 +0200

    Added cors to display the data on the client side

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const rooms = require("./routes/rooms");
 
 const app = express();
 
+const corsOptions = {
+  origin: "http://localhost:4200",
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
